d267bfd90cf5b9836aca6fdd9350eadcded24c0b
fbisquerra <fbisquerra@travelcompositor.com>
2020-04-02 01:34:06 +0200

    Fix it test

--- src/test/kotlin/com/ropitt/ropitt/it/PersonIT.kt
+++ src/test/kotlin/com/ropitt/ropitt/it/PersonIT.kt
@@ -1,33 +1,42 @@
 package com.ropitt.ropitt.it
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.graphql.spring.boot.test.GraphQLTestTemplate
+import com.ropitt.ropitt.entity.Person
 import com.ropitt.ropitt.repository.PersonRepository
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.ActiveProfiles
 
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("it")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PersonIT {
 
     @MockBean
     private lateinit var personRepository: PersonRepository
 
     @Autowired
-    private lateinit var restTemplate: TestRestTemplate
-
-    @LocalServerPort
-    private val port = 0
-    private val baseUrl = "http://localhost:"
+    private lateinit var graphQLTestTemplate: GraphQLTestTemplate
 
     @Test
     fun `get all users`() {
-        `when`(personRepository.findAll()).thenReturn(listOf())
+        `when`(personRepository.findAll()).thenReturn(listOf(Person("pepe")))
 
+        val variables = ObjectMapper().createObjectNode()
+        variables.put("count", "1")
+        val response = graphQLTestTemplate.perform("graphql/post-get-people.graphql", variables)
+
+        assertThat(response.readTree().toPrettyString())
+                .isEqualTo("{\n" +
+                            "  \"data\" : {\n" +
+                            "    \"people\" : [ {\n" +
+                            "      \"name\" : \"pepe\"\n" +
+                            "    } ]\n" +
+                            "  }\n" +
+                            "}")
     }
 }
